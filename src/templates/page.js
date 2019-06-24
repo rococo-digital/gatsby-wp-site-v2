30cfa0bf480142d44ba1a032964b34337dbdf84d
Matthew Jones <matt.lamellama@gmail.com>
2019-06-24 22:38:03 +0100

    hero subtitle

--- src/templates/page.js
+++ src/templates/page.js
@@ -2,8 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 
-import BackgroundImage from "gatsby-background-image-es5"
-import Tiles from "bulma/bulma.sass"
+import BackgroundImage from 'gatsby-background-image-es5'
+import Tiles from 'bulma/bulma.sass'
 import Layout from '../components/Layout'
 
 const Img = ({ objFit = `cover`, objPosition = `50% 50%`, ...props }) => (
@@ -18,32 +18,58 @@ const Img = ({ objFit = `cover`, objPosition = `50% 50%`, ...props }) => (
   />
 )
 
-export const PageTemplate = ({ title, content, featuredImage }) => {
+const bg_image = ({ objFit = `cover`, objPosition = `50% 50%`, ...props }) => (
+  <BackgroundImage
+    {...props}
+    imgStyle={{
+      ...props.imgStyle,
+      objectFit: objFit,
+      objectPosition: objPosition,
+      fontFamily: `"object-fit: ${objFit}; object-position: ${objPosition}"`,
+      backgroundImage: `url(${Background})`,
+    }}
+  />
+)
+
+export const PageTemplate = ({
+  title,
+  content,
+  featuredImage,
+  bgImage,
+  intro,
+  subtitle,
+}) => {
   return (
     <main>
-      {featuredImage ?
-
-        <Img tag="section" fluid={featuredImage.localFile.childImageSharp.fluid} backgroundColor={`#000`}>
+      {featuredImage ? (
+        <Img
+          tag="section"
+          fluid={featuredImage.localFile.childImageSharp.fluid}
+          backgroundColor={`#000`}
+        >
           <section class="hero">
-
             <div class="hero-body">
               <div class="container">
-                <h1 class="title has-text-white">
-                  {title}
-                </h1>
-                
+                <div class="tile is-ancestor">
+                  <div class="tile is-parent is-4">
+                    <article class="tile is-child box">
+                      <p class="title">{title}</p>
+                      <p class="subtitle">{subtitle}</p>
+                    </article>
+                  </div>
+                </div>
               </div>
             </div>
           </section>
         </Img>
+      ) : (
+        ''
+      )}
 
-        : ""}
-        
-          <div
-            className="is-fullwidth"
-            dangerouslySetInnerHTML={{ __html: content }}
-          />
-  
+      <div
+        className="is-fullwidth"
+        dangerouslySetInnerHTML={{ __html: content }}
+      />
     </main>
   )
 }
@@ -58,7 +84,14 @@ const Page = ({ data }) => {
 
   return (
     <Layout>
-      <PageTemplate title={page.title} content={page.content} featuredImage={page.featured_media} />
+      <PageTemplate
+        title={page.title}
+        content={page.content}
+        featuredImage={page.featured_media}
+        bgImage={page.acf.background_image}
+        intro={page.acf.intro_paragraph}
+        subtitle={page.acf.hero_subtitle}
+      />
     </Layout>
   )
 }
@@ -74,6 +107,20 @@ export const pageQuery = graphql`
     wordpressPage(id: { eq: $id }) {
       title
       content
+      acf {
+        hero_subtitle
+        intro_paragraph
+        background_image {
+          localFile {
+            childImageSharp {
+              fluid(quality: 90, maxWidth: 800) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+        }
+      }
+
       featured_media {
         localFile {
           childImageSharp {
