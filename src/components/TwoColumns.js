dc821b7ad86dd74aaedff49434c52a402bd02329
Matthew Jones <matt.lamellama@gmail.com>
2019-07-04 23:03:17 +0100

    fixed empty boxes

--- src/components/TwoColumns.js
+++ src/components/TwoColumns.js
@@ -3,33 +3,71 @@ import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import { ColumnBox } from './ColumnBox'
 
-export const TwoColumns = ({ text1, text2, text3, text4, title1, title2, title3, title4, image1, image2 }) => {
-  
-  return(
-  
-  <section id="side-by-side" className="section">
-    {text3 && text4 ? (
-    <div className="container is-fullhd">
-      
-      <div className="tile is-ancestor">
-        <ColumnBox title={title1} text={text1} image={image1} classes="has-background-dark tile is-parent"/>
-        <ColumnBox title={title2} text={text2} classes="has-background-grey-dark tile is-parent"/>
-      </div>
-      <div className="tile is-ancestor">
-        <ColumnBox title={title3} text={text3} classes="has-background-grey tile is-parent"/>
-        <ColumnBox title={title4} text={text4} image={image2} classes="has-background-primary tile is-parent"/>
-      </div>
-    </div>
+export const TwoColumns = ({
+  text1,
+  text2,
+  text3,
+  text4,
+  title1,
+  title2,
+  title3,
+  title4,
+  image1,
+  image2,
+}) => {
+  if(text1 && text2){
+  return (
+    <section id="side-by-side" className="section">
+      {text3 && text4 ? (
+        <div className="container is-fullhd">
+          <div className="tile is-ancestor">
+            <ColumnBox
+              title={title1}
+              text={text1}
+              image={image1}
+              classes="has-background-dark tile is-parent"
+            />
+            <ColumnBox
+              title={title2}
+              text={text2}
+              classes="has-background-grey-dark tile is-parent"
+            />
+          </div>
+          <div className="tile is-ancestor">
+            <ColumnBox
+              title={title3}
+              text={text3}
+              classes="has-background-grey tile is-parent"
+            />
+            <ColumnBox
+              title={title4}
+              text={text4}
+              image={image2}
+              classes="has-background-primary tile is-parent"
+            />
+          </div>
+        </div>
       ) : (
         <div className="container is-fullhd">
-      <div className="tile is-ancestor">
-        <ColumnBox title={title1} text={text1} image={image1} classes="has-background-dark tile is-parent"/>
-        <ColumnBox title={title2} text={text2} classes="has-background-grey-dark tile is-parent"/>
-      </div>
-      </div>
+          <div className="tile is-ancestor">
+            <ColumnBox
+              title={title1}
+              text={text1}
+              image={image1}
+              classes="has-background-dark tile is-parent"
+            />
+            <ColumnBox
+              title={title2}
+              text={text2}
+              classes="has-background-grey-dark tile is-parent"
+            />
+          </div>
+        </div>
       )}
-
-  </section>
-)}
+    </section>
+  )}
+  return(false)
+ 
+}
 
 export default TwoColumns
