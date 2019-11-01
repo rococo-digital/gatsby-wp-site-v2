061189390ed8468ccdf9b2a99969ec194cf4178d
Matthew Jones <matt.lamellama@gmail.com>
2019-11-01 20:55:57 +0000

    schema reverted and added new listitems

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,7 @@ import MainMenu from './MainMenu'
 import Footer from './Footer'
 import favicon from '../img/favicon.png'
 
-const schemaOrgWebPage = {
+const schemaOrgWebPageMenu = {
   "@context": "https://schema.org/", 
   "@type": "BreadcrumbList", 
   "itemListElement": [{
@@ -25,6 +25,35 @@ const schemaOrgWebPage = {
     "name": "sexual assault",
     "item": "https://iydl.co.uk/sexual-offences/sexual-assault/"  
   }]
+}
+
+const schemaOrgWebPage = {
+  "@context": "http://schema.org",
+     "@type": "LegalService",
+     "name": "In Your Defence Limited",
+     "description": "Criminal Defence Solicitors based in Crawley, West Sussex. Providing expert legal representation nationwide.",
+     "url": "https://iydl.co.uk/",
+     "image": "http://iydl.co.uk/static/iydl_logo-d22c35315967807cda4018b20cfc36cb.svg",
+     "priceRange": "£000 - £000",
+     "telephone": "+44 1293 550400",
+     "email": "support@iydl.co.uk",
+     "hasMap": "https://www.google.co.uk/maps/uv?hl=en&pb=!1s0x4875edc25b0d0195%3A0x5d012fb205379c80!2m22!2m2!1i80!2i80!3m1!2i20!16m16!1b1!2m2!1m1!1e1!2m2!1m1!1e3!2m2!1m1!1e5!2m2!1m1!1e4!2m2!1m1!1e6!3m1!7e115!4s%2Fmaps%2Fplace%2Fin%2Byour%2Bdefence%2Blimited%2F%4050.9883139%2C-0.2054967%2C3a%2C75y%2C344.48h%2C90t%2Fdata%3D*213m4*211e1*213m2*211ssS_QTh49cfz0eamWCyvYtA*212e0*214m2*213m1*211s0x4875edc25b0d0195%3A0x5d012fb205379c80%3Fsa%3DX!5sin%20your%20defence%20limited%20-%20Google%20Search!15sCAQ&imagekey=!1e2!2ssS_QTh49cfz0eamWCyvYtA&sa=X&ved=2ahUKEwiPzc7KzsrkAhXoRxUIHf7oBNMQpx8wCnoECA8QCw",
+     "address": {
+       "@type": "PostalAddress",
+       "addressLocality": "Bolney",
+       "addressRegion": "West sussex",
+       "postalCode": "RH17 5QT",
+       "streetAddress": "Bolney Place, Cowfold Road"
+     },
+     "geo": {
+       "@type": "GeoCoordinates",
+       "latitude": "50.988943",
+       "longitude": "-0.205844"
+     },
+     "sameAs": [
+       "https://www.facebook.com/inyourdefence",
+       "https://twitter.com/InYourDefenceUK"
+     ]
 }
 
 const TemplateWrapper = ({ siteUrl, slug, path, image, yoast, children }) => (
@@ -60,7 +89,8 @@ const TemplateWrapper = ({ siteUrl, slug, path, image, yoast, children }) => (
       {yoast.yoast_wpseo_twitter_image && <meta name="twitter:image" content={yoast.yoast_wpseo_twitter_image} />}
       <meta name="twitter:creator" content="@InYourDefenceUK" />
       {slug == "home" && <script type="application/ld+json">{JSON.stringify(schemaOrgWebPage)}</script>}
-
+      {slug == "home" && <script type="application/ld+json">{JSON.stringify(schemaOrgWebPageMenu)}</script>}
+ 
     </Helmet> :
     <Helmet title="In Your Defence"/>}
     <TopBar />
